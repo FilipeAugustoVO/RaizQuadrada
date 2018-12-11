76d0e6e5aacb8972545f954a9966262ec3aadf0b
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-12-11 19:47:28 -0300

    Add files via upload

---
+++ RaizQuadrada/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RaizQuadrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sqrt Program");
+            Console.Write("Sqrt of 123456 (Integer) : " + Math.Sqrt(12345));
+            
+        }
+    }
+}
